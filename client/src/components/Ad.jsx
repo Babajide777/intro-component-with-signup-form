3bf7ec399222a8d4aad253929165c0ee4cd4afac
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2021-11-26 12:55:55 -0600

    design complete

--- client/src/components/Ad.jsx
+++ client/src/components/Ad.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Ad = () => {
   return (
     <div className="ad">
-      <p>Try it free 7 days then $20/mo. thereafter</p>
+      <p>
+        <span>Try it free 7 days</span> then $20/mo. thereafter
+      </p>
     </div>
   );
 };
